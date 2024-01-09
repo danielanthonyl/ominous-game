b943d80eb87c3d5a4ae71cd1537d93e23b117a65
danielanthonyl <danskyjs@icloud.com>
2024-01-09 11:22:11 -0300

    added password encoding

--- src/main/java/com/darc/ominous/game/services/UserService.java
+++ src/main/java/com/darc/ominous/game/services/UserService.java
@@ -5,10 +5,7 @@ import java.util.UUID;
 
 import com.darc.ominous.game.utils.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.darc.ominous.game.mappers.UserMapper;
